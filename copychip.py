b8e9d3d90bcd40972c9ded6c9a73bab58888ba3a
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2025-06-19 16:18:46 +0530

    commit #1 - inital the file parse length

---
+++ copychip.py
@@ -0,0 +1,13 @@
+import os
+import argparse
+
+
+# ---------- Main module ----------
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 4:
+        print("Usage: python copychip.py <file> <start_line> [<end_line]")
+        sys.exit(1) 
+
+print("End of the project")
