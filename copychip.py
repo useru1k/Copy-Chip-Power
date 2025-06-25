beaeb3ffc68c88d7a11a6b46c3e633d8fb9f938a
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2025-06-25 22:51:43 +0530

    commit #2 add the --line copy to chipboard by tk

--- copychip.py
+++ copychip.py
@@ -1,13 +1,45 @@
 import os
 import argparse
+import tkinter as tk
 
+# ----------- Copy the Text from the File ------------
+def copy_from_line(file_path,line):
+    with open(file_path,'r') as f:
+        lines = f.readlines()
+    text = lines[line -1]
+    copy_to_chip(text)
+    print("Done the work")
+
+# ----------- Copy to Chipboard ---------------
+def copy_to_chip(text):
+    root = tk.Tk()
+    root.withdraw()
+    root.clipboard_clear()
+    root.clipboard_append(text)
+    root.update()
+    root.destroy()
 
 # ---------- Main module ----------
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) < 4:
-        print("Usage: python copychip.py <file> <start_line> [<end_line]")
-        sys.exit(1) 
+    parser = argparse.ArgumentParser(description='Copy the file to Cilpboard.')
+    parser.add_argument('--file',required=True)
+    parser.add_argument('--line', type=int)
+    parser.add_argument('--start', type=int)
+    parser.add_argument('--end', type=int)
 
-print("End of the project")
+    args = parser.parse_args();
+
+    if args.line:
+        copy_from_line(args.file,args.line)
+    if args.start and args.end:
+        copy_from_start_end(args.file,args.start,args.end)
+
+    #print("Hola form ChipPower")
+
+    #if len(sys.argv) < 4:
+    #   print("Usage: python copychip.py <file> <start_line> [<end_line]")
+    #   sys.exit(1) 
+
+#print("End of the project")
